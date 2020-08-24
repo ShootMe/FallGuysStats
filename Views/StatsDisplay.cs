938c5b88c4c1471073d48ef9814f38abea9dab7e
ShootMe <birreller@gmail.com>
2020-08-24 11:45:27 -0500

    More info

---
+++ Views/StatsDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+namespace FallGuysStats {
+    public partial class StatsDisplay : Form {
+        public DataTable Details { get; set; }
+        public StatsDisplay() {
+            InitializeComponent();
+        }
+
+        private void StatsDisplay_Load(object sender, EventArgs e) {
+            graph.DataSource = Details;
+        }
+    }
+}
