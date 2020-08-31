21cf761da973a9b21e08e3e68d61765cec8b5c21
ShootMe <birreller@gmail.com>
2020-08-30 20:27:23 -0500

    Improve overlay

--- Views/Overlay.Designer.cs
+++ Views/Overlay.Designer.cs
@@ -48,60 +48,54 @@
             // 
             this.lblRound.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblRound.AutoSize = true;
-            this.lblRound.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRound.Location = new System.Drawing.Point(51, 57);
             this.lblRound.Name = "lblRound";
-            this.lblRound.Size = new System.Drawing.Size(106, 16);
+            this.lblRound.Size = new System.Drawing.Size(76, 13);
             this.lblRound.TabIndex = 8;
             this.lblRound.Text = "Current Round";
             // 
             // lblNameDesc
             // 
             this.lblNameDesc.AutoSize = true;
-            this.lblNameDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNameDesc.Location = new System.Drawing.Point(6, 73);
             this.lblNameDesc.Name = "lblNameDesc";
-            this.lblNameDesc.Size = new System.Drawing.Size(48, 16);
+            this.lblNameDesc.Size = new System.Drawing.Size(38, 13);
             this.lblNameDesc.TabIndex = 9;
             this.lblNameDesc.Text = "Name:";
             // 
             // lblFastestDesc
             // 
             this.lblFastestDesc.AutoSize = true;
-            this.lblFastestDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFastestDesc.Location = new System.Drawing.Point(6, 121);
             this.lblFastestDesc.Name = "lblFastestDesc";
-            this.lblFastestDesc.Size = new System.Drawing.Size(93, 16);
+            this.lblFastestDesc.Size = new System.Drawing.Size(74, 13);
             this.lblFastestDesc.TabIndex = 15;
             this.lblFastestDesc.Text = "Fastest Finish:";
             // 
             // lblFinishDesc
             // 
             this.lblFinishDesc.AutoSize = true;
-            this.lblFinishDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFinishDesc.Location = new System.Drawing.Point(6, 153);
             this.lblFinishDesc.Name = "lblFinishDesc";
-            this.lblFinishDesc.Size = new System.Drawing.Size(46, 16);
+            this.lblFinishDesc.Size = new System.Drawing.Size(37, 13);
             this.lblFinishDesc.TabIndex = 19;
             this.lblFinishDesc.Text = "Finish:";
             // 
             // lblDurationDesc
             // 
             this.lblDurationDesc.AutoSize = true;
-            this.lblDurationDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDurationDesc.Location = new System.Drawing.Point(6, 137);
             this.lblDurationDesc.Name = "lblDurationDesc";
-            this.lblDurationDesc.Size = new System.Drawing.Size(61, 16);
+            this.lblDurationDesc.Size = new System.Drawing.Size(50, 13);
             this.lblDurationDesc.TabIndex = 17;
             this.lblDurationDesc.Text = "Duration:";
             // 
             // lblQualifyChanceDesc
             // 
             this.lblQualifyChanceDesc.AutoSize = true;
-            this.lblQualifyChanceDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblQualifyChanceDesc.Location = new System.Drawing.Point(6, 105);
             this.lblQualifyChanceDesc.Name = "lblQualifyChanceDesc";
-            this.lblQualifyChanceDesc.Size = new System.Drawing.Size(67, 16);
+            this.lblQualifyChanceDesc.Size = new System.Drawing.Size(53, 13);
             this.lblQualifyChanceDesc.TabIndex = 13;
             this.lblQualifyChanceDesc.Text = "Qualify %:";
             // 
@@ -109,10 +103,10 @@
             // 
             this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(60, 73);
+            this.lblName.AutoEllipsis = true;
+            this.lblName.Location = new System.Drawing.Point(63, 73);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(142, 16);
+            this.lblName.Size = new System.Drawing.Size(141, 16);
             this.lblName.TabIndex = 10;
             this.lblName.Text = "N/A";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -121,10 +115,9 @@
             // 
             this.lblDuration.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDuration.Location = new System.Drawing.Point(73, 137);
+            this.lblDuration.Location = new System.Drawing.Point(88, 137);
             this.lblDuration.Name = "lblDuration";
-            this.lblDuration.Size = new System.Drawing.Size(129, 16);
+            this.lblDuration.Size = new System.Drawing.Size(116, 16);
             this.lblDuration.TabIndex = 18;
             this.lblDuration.Text = "0:00";
             this.lblDuration.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -133,10 +126,9 @@
             // 
             this.lblFinish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFinish.Location = new System.Drawing.Point(58, 153);
+            this.lblFinish.Location = new System.Drawing.Point(67, 153);
             this.lblFinish.Name = "lblFinish";
-            this.lblFinish.Size = new System.Drawing.Size(144, 16);
+            this.lblFinish.Size = new System.Drawing.Size(137, 16);
             this.lblFinish.TabIndex = 20;
             this.lblFinish.Text = "0:00.00";
             this.lblFinish.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -145,10 +137,9 @@
             // 
             this.lblFastest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblFastest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFastest.Location = new System.Drawing.Point(105, 121);
+            this.lblFastest.Location = new System.Drawing.Point(123, 121);
             this.lblFastest.Name = "lblFastest";
-            this.lblFastest.Size = new System.Drawing.Size(97, 16);
+            this.lblFastest.Size = new System.Drawing.Size(81, 16);
             this.lblFastest.TabIndex = 16;
             this.lblFastest.Text = "0:00.00";
             this.lblFastest.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -157,10 +148,9 @@
             // 
             this.lblQualifyChance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblQualifyChance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQualifyChance.Location = new System.Drawing.Point(79, 105);
+            this.lblQualifyChance.Location = new System.Drawing.Point(92, 105);
             this.lblQualifyChance.Name = "lblQualifyChance";
-            this.lblQualifyChance.Size = new System.Drawing.Size(123, 16);
+            this.lblQualifyChance.Size = new System.Drawing.Size(112, 16);
             this.lblQualifyChance.TabIndex = 14;
             this.lblQualifyChance.Text = "100.0";
             this.lblQualifyChance.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -168,10 +158,9 @@
             // lblWinsDesc
             // 
             this.lblWinsDesc.AutoSize = true;
-            this.lblWinsDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWinsDesc.Location = new System.Drawing.Point(6, 5);
             this.lblWinsDesc.Name = "lblWinsDesc";
-            this.lblWinsDesc.Size = new System.Drawing.Size(41, 16);
+            this.lblWinsDesc.Size = new System.Drawing.Size(34, 13);
             this.lblWinsDesc.TabIndex = 0;
             this.lblWinsDesc.Text = "Wins:";
             // 
@@ -179,10 +168,9 @@
             // 
             this.lblWins.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWins.Location = new System.Drawing.Point(53, 5);
+            this.lblWins.Location = new System.Drawing.Point(58, 5);
             this.lblWins.Name = "lblWins";
-            this.lblWins.Size = new System.Drawing.Size(149, 16);
+            this.lblWins.Size = new System.Drawing.Size(146, 16);
             this.lblWins.TabIndex = 1;
             this.lblWins.Text = "0";
             this.lblWins.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -190,10 +178,9 @@
             // lblWinChanceDesc
             // 
             this.lblWinChanceDesc.AutoSize = true;
-            this.lblWinChanceDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWinChanceDesc.Location = new System.Drawing.Point(6, 21);
             this.lblWinChanceDesc.Name = "lblWinChanceDesc";
-            this.lblWinChanceDesc.Size = new System.Drawing.Size(49, 16);
+            this.lblWinChanceDesc.Size = new System.Drawing.Size(40, 13);
             this.lblWinChanceDesc.TabIndex = 2;
             this.lblWinChanceDesc.Text = "Win %:";
             // 
@@ -201,10 +188,9 @@
             // 
             this.lblWinChance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblWinChance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWinChance.Location = new System.Drawing.Point(61, 21);
+            this.lblWinChance.Location = new System.Drawing.Point(66, 21);
             this.lblWinChance.Name = "lblWinChance";
-            this.lblWinChance.Size = new System.Drawing.Size(141, 16);
+            this.lblWinChance.Size = new System.Drawing.Size(138, 16);
             this.lblWinChance.TabIndex = 3;
             this.lblWinChance.Text = "100.0";
             this.lblWinChance.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -212,10 +198,9 @@
             // lblFinalChanceDesc
             // 
             this.lblFinalChanceDesc.AutoSize = true;
-            this.lblFinalChanceDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFinalChanceDesc.Location = new System.Drawing.Point(6, 37);
             this.lblFinalChanceDesc.Name = "lblFinalChanceDesc";
-            this.lblFinalChanceDesc.Size = new System.Drawing.Size(55, 16);
+            this.lblFinalChanceDesc.Size = new System.Drawing.Size(43, 13);
             this.lblFinalChanceDesc.TabIndex = 4;
             this.lblFinalChanceDesc.Text = "Final %:";
             // 
@@ -223,10 +208,9 @@
             // 
             this.lblFinalChance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblFinalChance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFinalChance.Location = new System.Drawing.Point(67, 37);
+            this.lblFinalChance.Location = new System.Drawing.Point(74, 37);
             this.lblFinalChance.Name = "lblFinalChance";
-            this.lblFinalChance.Size = new System.Drawing.Size(135, 16);
+            this.lblFinalChance.Size = new System.Drawing.Size(130, 16);
             this.lblFinalChance.TabIndex = 5;
             this.lblFinalChance.Text = "100.0";
             this.lblFinalChance.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -235,10 +219,9 @@
             // 
             this.lblPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblPlayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayers.Location = new System.Drawing.Point(69, 89);
+            this.lblPlayers.Location = new System.Drawing.Point(78, 89);
             this.lblPlayers.Name = "lblPlayers";
-            this.lblPlayers.Size = new System.Drawing.Size(133, 16);
+            this.lblPlayers.Size = new System.Drawing.Size(126, 16);
             this.lblPlayers.TabIndex = 12;
             this.lblPlayers.Text = "0";
             this.lblPlayers.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -246,17 +229,15 @@
             // lblPlayersDesc
             // 
             this.lblPlayersDesc.AutoSize = true;
-            this.lblPlayersDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPlayersDesc.Location = new System.Drawing.Point(6, 89);
             this.lblPlayersDesc.Name = "lblPlayersDesc";
-            this.lblPlayersDesc.Size = new System.Drawing.Size(57, 16);
+            this.lblPlayersDesc.Size = new System.Drawing.Size(44, 13);
             this.lblPlayersDesc.TabIndex = 11;
             this.lblPlayersDesc.Text = "Players:";
             // 
             // Overlay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(207, 176);
             this.Controls.Add(this.lblPlayers);
