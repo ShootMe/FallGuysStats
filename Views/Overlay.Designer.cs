5dfa106ffbee6b8c9eaa9257abe34ba646f6c6e6
ShootMe <birreller@gmail.com>
2020-09-01 16:07:24 -0500

    Update overlay

--- Views/Overlay.Designer.cs
+++ Views/Overlay.Designer.cs
@@ -44,90 +44,87 @@
             // 
             // lblPlayers
             // 
-            this.lblPlayers.Location = new System.Drawing.Point(360, 33);
+            this.lblPlayers.Location = new System.Drawing.Point(359, 32);
             this.lblPlayers.Name = "lblPlayers";
-            this.lblPlayers.Size = new System.Drawing.Size(184, 24);
+            this.lblPlayers.Size = new System.Drawing.Size(198, 24);
             this.lblPlayers.TabIndex = 12;
             this.lblPlayers.Text = "0";
             this.lblPlayers.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblPlayersDesc
             // 
-            this.lblPlayersDesc.AutoSize = true;
-            this.lblPlayersDesc.Location = new System.Drawing.Point(262, 33);
+            this.lblPlayersDesc.Location = new System.Drawing.Point(260, 32);
             this.lblPlayersDesc.Name = "lblPlayersDesc";
-            this.lblPlayersDesc.Size = new System.Drawing.Size(111, 24);
+            this.lblPlayersDesc.Size = new System.Drawing.Size(106, 24);
             this.lblPlayersDesc.TabIndex = 11;
             this.lblPlayersDesc.Text = "PLAYERS:";
             // 
             // lblFinalChance
             // 
-            this.lblFinalChance.Location = new System.Drawing.Point(107, 32);
+            this.lblFinalChance.Location = new System.Drawing.Point(89, 31);
             this.lblFinalChance.Name = "lblFinalChance";
-            this.lblFinalChance.Size = new System.Drawing.Size(143, 24);
+            this.lblFinalChance.Size = new System.Drawing.Size(170, 24);
             this.lblFinalChance.TabIndex = 5;
             this.lblFinalChance.Text = "0 - 100.0%";
             this.lblFinalChance.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblFinalChanceDesc
             // 
-            this.lblFinalChanceDesc.AutoSize = true;
-            this.lblFinalChanceDesc.Location = new System.Drawing.Point(27, 33);
+            this.lblFinalChanceDesc.Location = new System.Drawing.Point(21, 32);
             this.lblFinalChanceDesc.Name = "lblFinalChanceDesc";
-            this.lblFinalChanceDesc.Size = new System.Drawing.Size(91, 24);
+            this.lblFinalChanceDesc.Size = new System.Drawing.Size(73, 24);
             this.lblFinalChanceDesc.TabIndex = 4;
-            this.lblFinalChanceDesc.Text = "FINALS:";
+            this.lblFinalChanceDesc.Text = "FINAL:";
             // 
             // lblWins
             // 
-            this.lblWins.Location = new System.Drawing.Point(92, 9);
+            this.lblWins.Location = new System.Drawing.Point(85, 8);
             this.lblWins.Name = "lblWins";
-            this.lblWins.Size = new System.Drawing.Size(158, 24);
+            this.lblWins.Size = new System.Drawing.Size(174, 24);
             this.lblWins.TabIndex = 1;
-            this.lblWins.Text = "0 - 0.0%";
+            this.lblWins.Text = "0 - 100.0%";
             this.lblWins.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblWinsDesc
             // 
-            this.lblWinsDesc.AutoSize = true;
-            this.lblWinsDesc.Location = new System.Drawing.Point(27, 9);
+            this.lblWinsDesc.Location = new System.Drawing.Point(21, 8);
             this.lblWinsDesc.Name = "lblWinsDesc";
-            this.lblWinsDesc.Size = new System.Drawing.Size(74, 24);
+            this.lblWinsDesc.Size = new System.Drawing.Size(69, 24);
             this.lblWinsDesc.TabIndex = 0;
             this.lblWinsDesc.Text = "WINS:";
             // 
             // lblQualifyChance
             // 
-            this.lblQualifyChance.Location = new System.Drawing.Point(356, 56);
+            this.lblQualifyChance.Location = new System.Drawing.Point(354, 55);
             this.lblQualifyChance.Name = "lblQualifyChance";
-            this.lblQualifyChance.Size = new System.Drawing.Size(188, 24);
+            this.lblQualifyChance.Size = new System.Drawing.Size(203, 24);
             this.lblQualifyChance.TabIndex = 14;
-            this.lblQualifyChance.Text = "100.0";
+            this.lblQualifyChance.Text = "0 / 0 - 0.0%";
             this.lblQualifyChance.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblFastest
             // 
-            this.lblFastest.Location = new System.Drawing.Point(656, 8);
+            this.lblFastest.Location = new System.Drawing.Point(655, 7);
             this.lblFastest.Name = "lblFastest";
-            this.lblFastest.Size = new System.Drawing.Size(124, 24);
+            this.lblFastest.Size = new System.Drawing.Size(128, 24);
             this.lblFastest.TabIndex = 16;
             this.lblFastest.Text = "0:00.00";
             this.lblFastest.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblFinish
             // 
-            this.lblFinish.Location = new System.Drawing.Point(637, 56);
+            this.lblFinish.Location = new System.Drawing.Point(635, 55);
             this.lblFinish.Name = "lblFinish";
-            this.lblFinish.Size = new System.Drawing.Size(143, 24);
+            this.lblFinish.Size = new System.Drawing.Size(148, 24);
             this.lblFinish.TabIndex = 20;
-            this.lblFinish.Text = "0:00.00";
+            this.lblFinish.Text = "#0 - 0:00.00";
             this.lblFinish.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblDuration
             // 
-            this.lblDuration.Location = new System.Drawing.Point(621, 32);
+            this.lblDuration.Location = new System.Drawing.Point(619, 31);
             this.lblDuration.Name = "lblDuration";
-            this.lblDuration.Size = new System.Drawing.Size(159, 24);
+            this.lblDuration.Size = new System.Drawing.Size(164, 24);
             this.lblDuration.TabIndex = 18;
             this.lblDuration.Text = "0:00";
             this.lblDuration.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -135,63 +132,58 @@
             // lblName
             // 
             this.lblName.AutoEllipsis = true;
-            this.lblName.Location = new System.Drawing.Point(332, 9);
+            this.lblName.Location = new System.Drawing.Point(328, 8);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(212, 24);
+            this.lblName.Size = new System.Drawing.Size(229, 24);
             this.lblName.TabIndex = 10;
             this.lblName.Text = "N/A";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // lblQualifyChanceDesc
             // 
-            this.lblQualifyChanceDesc.AutoSize = true;
-            this.lblQualifyChanceDesc.Location = new System.Drawing.Point(262, 56);
+            this.lblQualifyChanceDesc.Location = new System.Drawing.Point(260, 55);
             this.lblQualifyChanceDesc.Name = "lblQualifyChanceDesc";
-            this.lblQualifyChanceDesc.Size = new System.Drawing.Size(107, 24);
+            this.lblQualifyChanceDesc.Size = new System.Drawing.Size(100, 24);
             this.lblQualifyChanceDesc.TabIndex = 13;
             this.lblQualifyChanceDesc.Text = "QUALIFY:";
             // 
             // lblDurationDesc
             // 
-            this.lblDurationDesc.AutoSize = true;
-            this.lblDurationDesc.Location = new System.Drawing.Point(558, 32);
+            this.lblDurationDesc.Location = new System.Drawing.Point(558, 31);
             this.lblDurationDesc.Name = "lblDurationDesc";
-            this.lblDurationDesc.Size = new System.Drawing.Size(69, 24);
+            this.lblDurationDesc.Size = new System.Drawing.Size(63, 24);
             this.lblDurationDesc.TabIndex = 17;
             this.lblDurationDesc.Text = "TIME:";
             // 
             // lblFinishDesc
             // 
-            this.lblFinishDesc.AutoSize = true;
-            this.lblFinishDesc.Location = new System.Drawing.Point(558, 56);
+            this.lblFinishDesc.Location = new System.Drawing.Point(558, 55);
             this.lblFinishDesc.Name = "lblFinishDesc";
-            this.lblFinishDesc.Size = new System.Drawing.Size(88, 24);
+            this.lblFinishDesc.Size = new System.Drawing.Size(81, 24);
             this.lblFinishDesc.TabIndex = 19;
             this.lblFinishDesc.Text = "FINISH:";
             // 
             // lblFastestDesc
             // 
-            this.lblFastestDesc.AutoSize = true;
-            this.lblFastestDesc.Location = new System.Drawing.Point(558, 8);
+            this.lblFastestDesc.Location = new System.Drawing.Point(558, 7);
             this.lblFastestDesc.Name = "lblFastestDesc";
-            this.lblFastestDesc.Size = new System.Drawing.Size(112, 24);
+            this.lblFastestDesc.Size = new System.Drawing.Size(103, 24);
             this.lblFastestDesc.TabIndex = 15;
             this.lblFastestDesc.Text = "FASTEST:";
             // 
             // lblNameDesc
             // 
-            this.lblNameDesc.AutoSize = true;
-            this.lblNameDesc.Location = new System.Drawing.Point(262, 9);
+            this.lblNameDesc.Location = new System.Drawing.Point(260, 8);
             this.lblNameDesc.Name = "lblNameDesc";
-            this.lblNameDesc.Size = new System.Drawing.Size(77, 24);
+            this.lblNameDesc.Size = new System.Drawing.Size(73, 24);
             this.lblNameDesc.TabIndex = 9;
             this.lblNameDesc.Text = "NAME:";
             // 
             // lblRound
             // 
-            this.lblRound.Location = new System.Drawing.Point(27, 56);
+            this.lblRound.Location = new System.Drawing.Point(21, 55);
             this.lblRound.Name = "lblRound";
-            this.lblRound.Size = new System.Drawing.Size(223, 24);
+            this.lblRound.Size = new System.Drawing.Size(238, 24);
             this.lblRound.TabIndex = 8;
             this.lblRound.Text = "CURRENT ROUND";
             this.lblRound.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -202,25 +194,24 @@
             this.BackColor = System.Drawing.Color.Magenta;
             this.BackgroundImage = global::FallGuysStats.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(786, 99);
-            this.Controls.Add(this.lblPlayers);
             this.Controls.Add(this.lblPlayersDesc);
             this.Controls.Add(this.lblFinalChance);
             this.Controls.Add(this.lblFinalChanceDesc);
-            this.Controls.Add(this.lblWins);
             this.Controls.Add(this.lblWinsDesc);
-            this.Controls.Add(this.lblQualifyChance);
-            this.Controls.Add(this.lblFastest);
-            this.Controls.Add(this.lblFinish);
-            this.Controls.Add(this.lblDuration);
-            this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblQualifyChanceDesc);
             this.Controls.Add(this.lblDurationDesc);
             this.Controls.Add(this.lblFinishDesc);
             this.Controls.Add(this.lblFastestDesc);
             this.Controls.Add(this.lblNameDesc);
             this.Controls.Add(this.lblRound);
+            this.Controls.Add(this.lblQualifyChance);
+            this.Controls.Add(this.lblFastest);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.lblPlayers);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblWins);
+            this.Controls.Add(this.lblFinish);
             this.Cursor = System.Windows.Forms.Cursors.SizeAll;
-            this.Font = new System.Drawing.Font("Titan One", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.KeyPreview = true;
@@ -234,7 +225,6 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Overlay_KeyDown);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Overlay_MouseDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
