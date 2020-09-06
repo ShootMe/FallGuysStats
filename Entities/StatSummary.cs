e1d472b6dae353c206a7159ef9e75cde499b4a4a
ShootMe <birreller@gmail.com>
2020-09-05 21:27:13 -0500

    Add gold stats to overlay

--- Entities/StatSummary.cs
+++ Entities/StatSummary.cs
@@ -6,6 +6,7 @@ namespace FallGuysStats {
         public int BestStreak { get; set; }
         public int TotalPlays { get; set; }
         public int TotalQualify { get; set; }
+        public int TotalGolds { get; set; }
         public TimeSpan? BestFinish { get; set; }
         public TimeSpan? LongestFinish { get; set; }
         public int? BestScore { get; set; }
