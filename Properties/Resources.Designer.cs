60be91f93148a53c72f5ae0200e4f139b796d693
ShootMe <birreller@gmail.com>
2020-09-15 21:56:23 -0500

    Added export options for html and bbcode

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace FallGuysStats.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap export {
+            get {
+                object obj = ResourceManager.GetObject("export", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap info {
             get {
                 object obj = ResourceManager.GetObject("info", resourceCulture);
