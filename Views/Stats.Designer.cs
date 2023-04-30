26aab4c80c7064be01597977ebfe94cd661bbdfa
eunma <terro21c@gmail.com>
2023-05-01 08:23:08 +0900

    Add Fallalytics

--- Views/Stats.Designer.cs
+++ Views/Stats.Designer.cs
@@ -407,7 +407,7 @@ namespace FallGuysStats {
             // 
             this.trayCMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.trayOverlay, this.traySeparator1, this.traySettings, this.traySeparator2, this.trayFilters, this.trayProfile, this.traySeparator3, this.trayUpdate, this.trayHelp, this.trayLaunchFallGuys, this.traySeparator4, this.trayExitProgram });
             this.trayCMenu.Name = "trayCMenu";
-            this.trayCMenu.Size = new System.Drawing.Size(166, 226);
+            this.trayCMenu.Size = new System.Drawing.Size(166, 204);
             // 
             // trayOverlay
             // 
