e0a92f707eb1ad3b05fd93c6083136319a9acffe
eunma <terro21c@gmail.com>
2023-05-03 06:11:12 +0900

    UI update

--- Entities/LogFileWatcher.cs
+++ Entities/LogFileWatcher.cs
@@ -367,9 +367,9 @@ namespace FallGuysStats {
                     this.StatsForm.SetLinkedProfile(this.selectedShowId, logRound.PrivateLobby);
                 }
             }
-            else if (line.Line.IndexOf("[HandleSuccessfulLogin] Session: ", StringComparison.OrdinalIgnoreCase) > 0) {
+            else if ((index = line.Line.IndexOf("[HandleSuccessfulLogin] Session: ", StringComparison.OrdinalIgnoreCase)) > 0) {
                 //Store SessionID to prevent duplicates
-                this.sessionId = line.Line.Substring(line.Line.IndexOf("[HandleSuccessfulLogin] Session: ", StringComparison.OrdinalIgnoreCase) + 33);
+                this.sessionId = line.Line.Substring(index + 33);
             }
             else if ((index = line.Line.IndexOf("[StateGameLoading] Loading game level scene", StringComparison.OrdinalIgnoreCase)) > 0) {
                 logRound.Info = new RoundInfo { ShowNameId = this.selectedShowId, SessionId = this.sessionId };
