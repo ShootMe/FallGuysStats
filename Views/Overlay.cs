cb7576f2453d37c2f3499985d4fa4bffc9edafb9
eunma <terro21c@gmail.com>
2023-05-19 19:16:34 +0900

    Bug fix

--- Views/Overlay.cs
+++ Views/Overlay.cs
@@ -714,7 +714,7 @@ namespace FallGuysStats {
                         this.lblRound.ImageWidth = 0;
                         this.lblRound.ImageHeight = 0;
                     }
-                    this.lblRound.TextRight = roundName;
+                    this.lblRound.TextRight = this.StatsForm.GetRoundNameFromShareCode(roundName);
 
                     this.lblWins.Text = $"{Multilingual.GetWord("overlay_wins")} :";
                     float winChance = levelInfo.TotalWins * 100f / (levelInfo.TotalShows == 0 ? 1 : levelInfo.TotalShows);
