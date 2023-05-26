07a70557846559b8e93ae11d2c2f0a484f1d1007
eunma <terro21c@gmail.com>
2023-05-26 16:34:38 +0900

    Bugfix and program optimization

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -462,6 +462,16 @@ namespace FallGuysStats.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fallguys_db_logo {
+            get {
+                object obj = ResourceManager.GetObject("fallguys_db_logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap fallguys_icon {
             get {
                 object obj = ResourceManager.GetObject("fallguys_icon", resourceCulture);
