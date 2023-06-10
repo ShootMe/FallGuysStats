5e47ca4c2568f504138d5215fc98a5b0a21a542b
eunma <terro21c@gmail.com>
2023-06-10 09:41:32 +0900

    Add information to statistics graph by round

--- Views/RoundStatsDisplay.Designer.cs
+++ Views/RoundStatsDisplay.Designer.cs
@@ -29,6 +29,9 @@
             this.picRoundIcon = new System.Windows.Forms.PictureBox();
             this.cboRoundList = new MetroFramework.Controls.MetroComboBox();
             this.lblRoundTime = new MetroFramework.Controls.MetroLabel();
+            this.lblRoundType = new FallGuysStats.RoundLabel();
+            this.lblBestRecord = new MetroFramework.Controls.MetroLabel();
+            this.lblWorstRecord = new MetroFramework.Controls.MetroLabel();
             this.picGoldMedalIcon = new System.Windows.Forms.PictureBox();
             this.lblCountGoldMedal = new System.Windows.Forms.Label();
             this.picSilverMedalIcon = new System.Windows.Forms.PictureBox();
@@ -59,7 +62,7 @@
             // picRoundIcon
             // 
             this.picRoundIcon.Image = global::FallGuysStats.Properties.Resources.fallguys_icon;
-            this.picRoundIcon.Location = new System.Drawing.Point(616, 63);
+            this.picRoundIcon.Location = new System.Drawing.Point(616, 64);
             this.picRoundIcon.Name = "picRoundIcon";
             this.picRoundIcon.Size = new System.Drawing.Size(60, 60);
             this.picRoundIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -92,12 +95,47 @@
             this.lblRoundTime.Text = "00:00:00";
             this.lblRoundTime.UseCustomForeColor = true;
             // 
+            // lblRoundType
+            // 
+            this.lblRoundType.ForeColor = System.Drawing.Color.White;
+            this.lblRoundType.Location = new System.Drawing.Point(616, 138);
+            this.lblRoundType.Name = "lblRoundType";
+            this.lblRoundType.Size = new System.Drawing.Size(100, 35);
+            this.lblRoundType.TabIndex = 15;
+            this.lblRoundType.Text = "RACE";
+            // 
+            // lblBestRecord
+            // 
+            this.lblBestRecord.AutoSize = true;
+            this.lblBestRecord.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblBestRecord.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblBestRecord.ForeColor = System.Drawing.Color.FromArgb(218, 165, 32);
+            this.lblBestRecord.Location = new System.Drawing.Point(722, 138);
+            this.lblBestRecord.Name = "lblBestRecord";
+            this.lblBestRecord.Size = new System.Drawing.Size(82, 25);
+            this.lblBestRecord.TabIndex = 16;
+            this.lblBestRecord.Text = "0";
+            this.lblBestRecord.UseCustomForeColor = true;
+            // 
+            // lblWorstRecord
+            // 
+            this.lblWorstRecord.AutoSize = true;
+            this.lblWorstRecord.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblWorstRecord.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblWorstRecord.ForeColor = System.Drawing.Color.FromArgb(128, 0, 128);
+            this.lblWorstRecord.Location = new System.Drawing.Point(722, 163);
+            this.lblWorstRecord.Name = "lblWorstRecord";
+            this.lblWorstRecord.Size = new System.Drawing.Size(82, 25);
+            this.lblWorstRecord.TabIndex = 17;
+            this.lblWorstRecord.Text = "0";
+            this.lblWorstRecord.UseCustomForeColor = true;
+            // 
             // picGoldMedalIcon
             // 
             this.picGoldMedalIcon.Image = global::FallGuysStats.Properties.Resources.medal_gold_original;
-            this.picGoldMedalIcon.Location = new System.Drawing.Point(616, 163);
+            this.picGoldMedalIcon.Location = new System.Drawing.Point(616, 211);
             this.picGoldMedalIcon.Name = "picGoldMedalIcon";
-            this.picGoldMedalIcon.Size = new System.Drawing.Size(60, 60);
+            this.picGoldMedalIcon.Size = new System.Drawing.Size(55, 55);
             this.picGoldMedalIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picGoldMedalIcon.TabIndex = 5;
             this.picGoldMedalIcon.TabStop = false;
@@ -105,18 +143,18 @@
             // lblCountGoldMedal
             // 
             this.lblCountGoldMedal.ForeColor = System.Drawing.Color.Teal;
-            this.lblCountGoldMedal.Location = new System.Drawing.Point(700, 162);
+            this.lblCountGoldMedal.Location = new System.Drawing.Point(700, 211);
             this.lblCountGoldMedal.Name = "lblCountGoldMedal";
-            this.lblCountGoldMedal.Size = new System.Drawing.Size(200, 58);
+            this.lblCountGoldMedal.Size = new System.Drawing.Size(200, 54);
             this.lblCountGoldMedal.TabIndex = 6;
             this.lblCountGoldMedal.Text = "0";
             // 
             // picSilverMedalIcon
             // 
             this.picSilverMedalIcon.Image = global::FallGuysStats.Properties.Resources.medal_silver_original;
-            this.picSilverMedalIcon.Location = new System.Drawing.Point(616, 243);
+            this.picSilverMedalIcon.Location = new System.Drawing.Point(616, 280);
             this.picSilverMedalIcon.Name = "picSilverMedalIcon";
-            this.picSilverMedalIcon.Size = new System.Drawing.Size(60, 60);
+            this.picSilverMedalIcon.Size = new System.Drawing.Size(55, 55);
             this.picSilverMedalIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picSilverMedalIcon.TabIndex = 7;
             this.picSilverMedalIcon.TabStop = false;
@@ -124,18 +162,18 @@
             // lblCountSilverMedal
             // 
             this.lblCountSilverMedal.ForeColor = System.Drawing.Color.Teal;
-            this.lblCountSilverMedal.Location = new System.Drawing.Point(700, 242);
+            this.lblCountSilverMedal.Location = new System.Drawing.Point(700, 280);
             this.lblCountSilverMedal.Name = "lblCountSilverMedal";
-            this.lblCountSilverMedal.Size = new System.Drawing.Size(200, 58);
+            this.lblCountSilverMedal.Size = new System.Drawing.Size(200, 54);
             this.lblCountSilverMedal.TabIndex = 8;
             this.lblCountSilverMedal.Text = "0";
             // 
             // picBronzeMedalIcon
             // 
             this.picBronzeMedalIcon.Image = global::FallGuysStats.Properties.Resources.medal_bronze_original;
-            this.picBronzeMedalIcon.Location = new System.Drawing.Point(616, 323);
+            this.picBronzeMedalIcon.Location = new System.Drawing.Point(616, 349);
             this.picBronzeMedalIcon.Name = "picBronzeMedalIcon";
-            this.picBronzeMedalIcon.Size = new System.Drawing.Size(60, 60);
+            this.picBronzeMedalIcon.Size = new System.Drawing.Size(55, 55);
             this.picBronzeMedalIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picBronzeMedalIcon.TabIndex = 9;
             this.picBronzeMedalIcon.TabStop = false;
@@ -143,18 +181,18 @@
             // lblCountBronzeMedal
             // 
             this.lblCountBronzeMedal.ForeColor = System.Drawing.Color.Teal;
-            this.lblCountBronzeMedal.Location = new System.Drawing.Point(700, 322);
+            this.lblCountBronzeMedal.Location = new System.Drawing.Point(700, 349);
             this.lblCountBronzeMedal.Name = "lblCountBronzeMedal";
-            this.lblCountBronzeMedal.Size = new System.Drawing.Size(200, 58);
+            this.lblCountBronzeMedal.Size = new System.Drawing.Size(200, 54);
             this.lblCountBronzeMedal.TabIndex = 10;
             this.lblCountBronzeMedal.Text = "0";
             // 
             // picPinkMedalIcon
             // 
             this.picPinkMedalIcon.Image = global::FallGuysStats.Properties.Resources.medal_pink_original;
-            this.picPinkMedalIcon.Location = new System.Drawing.Point(616, 403);
+            this.picPinkMedalIcon.Location = new System.Drawing.Point(616, 418);
             this.picPinkMedalIcon.Name = "picPinkMedalIcon";
-            this.picPinkMedalIcon.Size = new System.Drawing.Size(60, 60);
+            this.picPinkMedalIcon.Size = new System.Drawing.Size(55, 55);
             this.picPinkMedalIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picPinkMedalIcon.TabIndex = 11;
             this.picPinkMedalIcon.TabStop = false;
@@ -162,18 +200,18 @@
             // lblCountPinkMedal
             // 
             this.lblCountPinkMedal.ForeColor = System.Drawing.Color.Teal;
-            this.lblCountPinkMedal.Location = new System.Drawing.Point(700, 402);
+            this.lblCountPinkMedal.Location = new System.Drawing.Point(700, 418);
             this.lblCountPinkMedal.Name = "lblCountPinkMedal";
-            this.lblCountPinkMedal.Size = new System.Drawing.Size(200, 58);
+            this.lblCountPinkMedal.Size = new System.Drawing.Size(200, 54);
             this.lblCountPinkMedal.TabIndex = 12;
             this.lblCountPinkMedal.Text = "0";
             // 
             // picEliminatedMedalIcon
             // 
             this.picEliminatedMedalIcon.Image = global::FallGuysStats.Properties.Resources.medal_eliminated_original;
-            this.picEliminatedMedalIcon.Location = new System.Drawing.Point(616, 483);
+            this.picEliminatedMedalIcon.Location = new System.Drawing.Point(616, 487);
             this.picEliminatedMedalIcon.Name = "picEliminatedMedalIcon";
-            this.picEliminatedMedalIcon.Size = new System.Drawing.Size(60, 60);
+            this.picEliminatedMedalIcon.Size = new System.Drawing.Size(55, 55);
             this.picEliminatedMedalIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picEliminatedMedalIcon.TabIndex = 13;
             this.picEliminatedMedalIcon.TabStop = false;
@@ -181,9 +219,9 @@
             // lblCountEliminatedMedal
             // 
             this.lblCountEliminatedMedal.ForeColor = System.Drawing.Color.Teal;
-            this.lblCountEliminatedMedal.Location = new System.Drawing.Point(700, 482);
+            this.lblCountEliminatedMedal.Location = new System.Drawing.Point(700, 487);
             this.lblCountEliminatedMedal.Name = "lblCountEliminatedMedal";
-            this.lblCountEliminatedMedal.Size = new System.Drawing.Size(200, 58);
+            this.lblCountEliminatedMedal.Size = new System.Drawing.Size(200, 54);
             this.lblCountEliminatedMedal.TabIndex = 14;
             this.lblCountEliminatedMedal.Text = "0";
             // 
@@ -196,6 +234,9 @@
             this.Controls.Add(this.picRoundIcon);
             this.Controls.Add(this.cboRoundList);
             this.Controls.Add(this.lblRoundTime);
+            this.Controls.Add(this.lblRoundType);
+            this.Controls.Add(this.lblBestRecord);
+            this.Controls.Add(this.lblWorstRecord);
             this.Controls.Add(this.picGoldMedalIcon);
             this.Controls.Add(this.lblCountGoldMedal);
             this.Controls.Add(this.picSilverMedalIcon);
@@ -218,8 +259,8 @@
             this.ShowInTaskbar = false;
             this.Style = MetroFramework.MetroColorStyle.Teal;
             this.Text = "Round Stats Display";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RoundStatsDisplay_KeyDown);
             this.Load += new System.EventHandler(this.RoundStatsDisplay_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RoundStatsDisplay_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.picRoundIcon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picGoldMedalIcon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picSilverMedalIcon)).EndInit();
@@ -229,12 +270,15 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
-        
+
         #endregion
         private ScottPlot.FormsPlot formsPlot;
         private System.Windows.Forms.PictureBox picRoundIcon;
         private MetroFramework.Controls.MetroComboBox cboRoundList;
         private MetroFramework.Controls.MetroLabel lblRoundTime;
+        private RoundLabel lblRoundType;
+        private MetroFramework.Controls.MetroLabel lblBestRecord;
+        private MetroFramework.Controls.MetroLabel lblWorstRecord;
         private System.Windows.Forms.PictureBox picGoldMedalIcon;
         private System.Windows.Forms.Label lblCountGoldMedal;
         private System.Windows.Forms.PictureBox picSilverMedalIcon;
