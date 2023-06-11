6575e43986a88c84d3c5fa60070b3325675fb713
eunma <terro21c@gmail.com>
2023-06-12 08:51:58 +0900

    Bug fix

--- Views/Stats.cs
+++ Views/Stats.cs
@@ -962,7 +962,7 @@ namespace FallGuysStats {
                 e.Graphics.DrawImage(
                     this.CurrentSettings.AutoChangeProfile ? Properties.Resources.link_on_icon :
                     this.Theme == MetroThemeStyle.Light ? Properties.Resources.link_icon :
-                    Properties.Resources.link_gray_icon, 20, 4, 13, 13);
+                    Properties.Resources.link_gray_icon, 20, 5, 11, 11);
             }
         }
         private void RemoveUpdateFiles() {
