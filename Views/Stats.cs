6b26213ad5a398250c2867d20e386eb331d33934
eunma <terro21c@gmail.com>
2023-06-28 20:02:13 +0900

    Bug fix

--- Views/Stats.cs
+++ Views/Stats.cs
@@ -1750,6 +1750,27 @@ namespace FallGuysStats {
                 this.CurrentSettings.Version = 42;
                 this.SaveUserSettings();
             }
+            
+            if (this.CurrentSettings.Version == 42) {
+                this.AllStats.AddRange(this.RoundDetails.FindAll());
+                this.StatsDB.BeginTrans();
+                this.CurrentSettings.NotifyServerConnected = false;
+                for (int i = this.AllStats.Count - 1; i >= 0; i--) {
+                    RoundInfo info = this.AllStats[i];
+                    if ("show_wle_s10_wk08_srs_01".Equals(info.ShowNameId, StringComparison.OrdinalIgnoreCase) ||
+                        "show_wle_s10_wk08_srs_02".Equals(info.ShowNameId, StringComparison.OrdinalIgnoreCase) ||
+                        "show_wle_s10_wk08_srs_03".Equals(info.ShowNameId, StringComparison.OrdinalIgnoreCase) ||
+                        "show_wle_s10_wk08_srs_04".Equals(info.ShowNameId, StringComparison.OrdinalIgnoreCase))
+                    {
+                        info.IsFinal = true;
+                        this.RoundDetails.Update(info);
+                    }
+                }
+                this.StatsDB.Commit();
+                this.AllStats.Clear();
+                this.CurrentSettings.Version = 43;
+                this.SaveUserSettings();
+            }
         }
         private UserSettings GetDefaultSettings() {
             return new UserSettings {
@@ -1821,7 +1842,7 @@ namespace FallGuysStats {
                 UpdatedDateFormat = true,
                 WinPerDayGraphStyle = 0,
                 Visible = true,
-                Version = 42
+                Version = 43
             };
         }
         private void UpdateHoopsieLegends() {
