213b640200149ad1268cbe4c911d61a6cc02dd22
eunma <terro21c@gmail.com>
2023-08-04 21:41:04 +0900

    1.238 version update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9ddf7bd8-3c77-43d4-a229-f79ce082c6e8")]
-[assembly: AssemblyVersion("1.237.0.0")]
-[assembly: AssemblyFileVersion("1.237.0.0")]
+[assembly: AssemblyVersion("1.238.0.0")]
+[assembly: AssemblyFileVersion("1.238.0.0")]
--- README.md
+++ README.md
@@ -237,6 +237,8 @@ FallGuysStats is open source, meaning anyone can view the code, so download it w
 <details>
     <summary>Details</summary>
 
+  - `1.238`
+    - Bugfix and program optimization
   - `1.237`
     - Bugfix and program optimization
   - `1.236`
