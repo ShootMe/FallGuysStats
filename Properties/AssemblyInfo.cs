af8672da4009dfe526a83e9df347ed16649752e3
eunma <terro21c@gmail.com>
2023-09-07 19:16:10 +0900

    1.259 version update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9ddf7bd8-3c77-43d4-a229-f79ce082c6e8")]
-[assembly: AssemblyVersion("1.258.0.0")]
-[assembly: AssemblyFileVersion("1.258.0.0")]
+[assembly: AssemblyVersion("1.259.0.0")]
+[assembly: AssemblyFileVersion("1.259.0.0")]
